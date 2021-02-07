9bb5fa5357196f281965c0b81b777e1cc463f3b0
Zak Kaioken <31756541+ZakKaioken@users.noreply.github.com>
2021-02-07 15:23:39 -0700

    Updated to .Net 5.0 with no problems

--- Abbybot-III/Program.cs
+++ Abbybot-III/Program.cs
@@ -1,4 +1,5 @@
-﻿using Abbybot_III.Apis.Mysql;
+﻿using Abbybot_III;
+using Abbybot_III.Apis.Mysql;
 using Abbybot_III.Apis.Twitter;
 using Abbybot_III.Core.AbbyBooru;
 using Abbybot_III.Core.Heart;
@@ -7,33 +8,22 @@ using Abbybot_III.Core.Twitter;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
-namespace Abbybot_III
+Abbybot.print("Abbybot III starting!");
+if (Process.GetProcessesByName("AbbybotSleep").Length < 1)
+    Process.Start("AbbybotSleep.exe");
+await MysqlCore.CheckMysql(@"ApiKeys\mysqlbinpath.abbytxt");
+AbbySql.AbbysqlClient.connectionstring = Abbybot_III.Apis.Mysql.ApiKeys.MysqlApiKeys.Load(@"ApiKeys\Mysql.json").ToString();
+await InitAll();
+await Abbybot_III.Apis.Discord.Discord.DiscordMainAsync();
+AbbyHeart.Start();
+while (true)
 {
-    class Program
-    {
-        static async Task Main()
-        {
-            Abbybot.print("Abbybot III starting!");
-            if (Process.GetProcessesByName("AbbybotSleep").Length < 1)
-                Process.Start("AbbybotSleep.exe");
-            await MysqlCore.CheckMysql(@"ApiKeys\mysqlbinpath.abbytxt");
-            AbbySql.AbbysqlClient.connectionstring = Apis.Mysql.ApiKeys.MysqlApiKeys.Load(@"ApiKeys\Mysql.json").ToString();
-            await InitAll();
-            await Apis.Discord.Discord.DiscordMainAsync();
-            AbbyHeart.Start();
-
-            while (true)
-            {
-                await Task.Delay(1);
-            }
-        }
-
-        static async Task InitAll()
-        {
-            await Twitter.init();
-            AbbyBooruChecker.Init();
-            if (Twitter.tson)
-                AbbybotTwitter.init();
-        }
-    }
+    await Task.Delay(1);
+}
+static async Task InitAll()
+{
+    await Twitter.init();
+    AbbyBooruChecker.Init();
+    if (Twitter.tson)
+        AbbybotTwitter.init();
 }
